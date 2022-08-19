f015b4e747a0db90fc75eace8dc23bc8ca1707b3
zhaza <Khail.ballin12@yahoo.com>
2022-08-19 02:50:48 -0700

    Add PageSnap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 // import { Link, animateScroll as scroll } from "react-scroll";
-import Home from './components/Home/Home'
-import Mentee from './components/Mentee/Nav'
-import Login from './components/Mentee/Login/Login'
 import './App.scss';
+import Home from './components/Home/Home'
+import Nav from './components/Mentee/Nav/Nav'
+import Login from './components/Mentee/Login/Login'
+import React, { useState } from 'react'
+import { WindowScroll, PageSnap } from 'react-easy-snap'
 
 function App() {
+
+  const [currentPageState, setCurrentPageState] = useState();
+
   return (
     <div className="App">
-      <Home />
-      <Mentee />
-      <Login />
+      <WindowScroll height='100vh' width='100vw' scrollDirection='y' timeScrollDisabled='200' mainPage='0'
+                  currentPageState={currentPageState} setCurrentPageState={setCurrentPageState}
+                  minWidth='960'>
+        <PageSnap>
+          <Home />
+        </PageSnap>
+        <Nav />
+        <PageSnap>
+          <Login />
+        </PageSnap>
+      </WindowScroll>
     </div>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div className='container'>
       <h1>Bulldog Dream Scholarship</h1>
+      { /* onClick go to next Sign-in section */ }
       <button className='startBtn'>
         START
         <br/>

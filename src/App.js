4cbbbf56d13978e7afa8e4683483618dce62b883
zhaza <Khail.ballin12@yahoo.com>
2022-08-21 17:44:35 -0700

    Remove scroll snap from app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-// import { Link, animateScroll as scroll } from "react-scroll";
 import './App.scss';
 import Home from './components/Home/Home'
 import Nav from './components/Mentee/Nav/Nav'
 import Login from './components/Mentee/Login/Login'
 import Info from './components/Mentee/Info/Info';
 import Docs from './components/Mentee/Docs/Docs';
-import React, { useRef } from 'react'
-import useScrollSnap from 'react-use-scroll-snap';
 
 function App() {
 
-  const scrollRef = useRef(null);
-  useScrollSnap({ ref: scrollRef, duration: 100, delay: 50 });
-
   return (
     <div className="App">
-        <section ref={scrollRef}>
-          <Home />
-          <Nav />
-          <Login />
-          <Info />
-          <Docs />
-        </section>
+        <Home />
+        <Nav />
+        <Login />
+        <Info />
+        <Docs />
     </div>
   );
 }
